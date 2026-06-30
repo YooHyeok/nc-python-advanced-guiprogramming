83e9b613a45399d98789d0ebbe79ba570c7eecaa
유재혁 <yjk7454@naver.com>
2026-07-01 00:04:13 +0900

    feat: `1.create_layout.py` - gui_project  기본 레이아웃3: 여백, 높이 조정 예제

--- gui_project/1.create_layout.py
+++ gui_project/1.create_layout.py
@@ -6,7 +6,7 @@ root.title("Nado GUI")
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
-file_frame.pack(fill="x") # B) x축 기준 간격 펼치기
+file_frame.pack(fill="x", padx=5, pady=5) # x축 기준 간격 펼치기 / A) 간격 띄우기 - pad
 
 btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일추가")
 btn_add_file.pack(side="left")
@@ -16,7 +16,7 @@ btn_del_file.pack(side="right")
 
 # 리스트 프레임
 list_frame=Frame(root)
-list_frame.pack(fill="both")
+list_frame.pack(fill="both", padx=5, pady=5)
 scrollbar = Scrollbar(list_frame)
 scrollbar.pack(side="right", fill="y") # fill y : 세로 공간을 꽉 채움(스크롤)
 
@@ -26,59 +26,59 @@ scrollbar.config(command=list_file.yview) # 스크롤에 리스트박스 매핑
 
 # 저장 경로 프레임
 path_frame = LabelFrame(root, text="저장경로")
-path_frame.pack(fill="x") # C) 저장경로 x축 기준 간격 펼치기
+path_frame.pack(fill="x", padx=5, pady=5, ipady=4) # 저장경로 x축 기준 간격 펼치기 / B) 프레임 높이 조정 - ipad
 
 txt_dest_path = Entry(path_frame, width=50)
-txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # iapy: 높이 조정
+txt_dest_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4) # iapy: 높이 조정 / A) 간격 띄우기 - pad
 
 btn_dest_path = Button(path_frame, text="찾아보기", width=10)
-btn_dest_path.pack(side="right")
+btn_dest_path.pack(side="right", padx=5, pady=5)
 
-# A) 옵션 프레임
+# 옵션 프레임
 frame_option = LabelFrame(root, text="옵션")
-frame_option.pack()
+frame_option.pack(padx=5, pady=5, ipady=4) # B) 프레임 높이 조정 - ipad
 
-## 1. 가로 넓이 옵션
-### 1-1. 가로 넓이 라벨
-Label(frame_option, text="가로넓이", width=8).pack(side="left")
-### 1-2. 가로 넓이 콤보
+## 가로 넓이 옵션
+### 가로 넓이 라벨
+Label(frame_option, text="가로넓이", width=8).pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
+### 가로 넓이 콤보
 opt_width=["원본 유지", "1024", "800", "640"]
 cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
 cmb_width.current(0)
-cmb_width.pack(side="left")
-## 2. 간격 옵션
-### 2-1. 간격 옵션 라벨
-Label(frame_option, text="간격", width=8).pack(side="left")
+cmb_width.pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
+## 간격 옵션
+### 간격 옵션 라벨
+Label(frame_option, text="간격", width=8).pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
 opt_space=["없음", "좁게", "보통", "넓게"]
 cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
 cmb_space.current(0)
-cmb_space.pack(side="left")
-### 2-2. 간격 옵션 라벨
+cmb_space.pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
+### 간격 옵션 라벨
 
-## 3. 파일 포맷 옵션
-Label(frame_option, text="포맷", width=8).pack(side="left")
+## 파일 포맷 옵션
+Label(frame_option, text="포맷", width=8).pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
 opt_format=["PNG", "JPG", "BMP"]
 cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
 cmb_format.current(0)
-cmb_format.pack(side="left")
+cmb_format.pack(side="left", padx=5, pady=5) # A) 간격 띄우기 - pad
 
-## D) 진행상황 Progress Bar
+## 진행상황 Progress Bar
 frame_progress = LabelFrame(root, text="진행상황")
-frame_progress.pack(fill="x")
+frame_progress.pack(fill="x", padx=5, pady=5, ipady=4) # B) 프레임 높이 조정 - ipad / B) 프레임 높이 조정 - ipad
 
 p_var = DoubleVar()
 progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
-progress_bar.pack(fill="x")
+progress_bar.pack(fill="x", padx=5, pady=5) # A) 간격 띄우기 - pad
 
-# E) 실행 프레임
+# 실행 프레임
 frame_run = Frame(root)
-frame_run.pack(fill="x")
+frame_run.pack(fill="x", padx=5, pady=5)
 
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
-btn_close.pack(side="right")
+btn_close.pack(side="right", padx=5, pady=5) # A) 간격 띄우기 - pad
 
 btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
-btn_start.pack(side="right")
+btn_start.pack(side="right", padx=5, pady=5) # A) 간격 띄우기 - pad
 
 root.resizable(False, False)
 root.mainloop()
