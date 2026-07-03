6d87b85a05edc6293ba6271aa053557f394eb8bc
유재혁 <yjk7454@naver.com>
2026-07-03 23:29:39 +0900

    feat: `2.basic_function.py` - gui_project  기본 기능2: 저장 경로선택 및 시작 검증 기능 구현 예제

--- gui_project/2.basic_function.py
+++ gui_project/2.basic_function.py
@@ -1,9 +1,36 @@
 from tkinter import *
 from tkinter import filedialog
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 
 root = Tk()
 root.title("Nado GUI")
+
+# C) 저장경로 (폴더)
+def browse_dest_path():
+  folder_selected = filedialog.askdirectory() # 폴더 선택 후 반환받은 폴더 경로 저장
+  if folder_selected == None: # 사용자가 취소를 누를 때
+    return
+  # print(folder_selected)
+  txt_dest_path.delete(0, END) #  경로 초기화(Entry 이므로 0부터 끝까지 삭제)
+  txt_dest_path.insert(0, folder_selected) #  선택된 경로 삽입
+
+# D) 시작: 병합작업(파일목록, 저장경로, 옵션 정보 기준)
+def start():
+  # 각 옵션들 값을 확인
+  print("가로넓이 : ", cmb_width.get())
+  print("간격 : ", cmb_space.get())
+  print("포맷 : ", cmb_format.get())
+
+  # 파일 목록 조회
+  if list_file.size() == 0:
+    msgbox.showwarning("경고", "이미지 파일을 추가하세요")
+    return
+  # 저장 경로 확인
+  if len(txt_dest_path.get()) == 0:
+    msgbox.showwarning("경고", "저장 경로를 선택하세요")
+    return
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
@@ -50,7 +77,7 @@ path_frame.pack(fill="x", padx=5, pady=5, ipady=4)
 txt_dest_path = Entry(path_frame, width=50)
 txt_dest_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4)
 
-btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browse_dest_path)
 btn_dest_path.pack(side="right", padx=5, pady=5)
 
 # 옵션 프레임
@@ -96,7 +123,7 @@ frame_run.pack(fill="x", padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
 btn_close.pack(side="right", padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
 
 root.resizable(False, False)
